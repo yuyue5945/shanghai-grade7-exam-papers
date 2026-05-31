396964ac012463f0d1971051c190ebc6906e7568
yuyue5945 <yuyue5945@gmail.com>
2026-05-31 19:50:01 +0800

    add: resource index and download guide

---
+++ scripts/download-from-quark.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 夸克网盘下载助手
+# 使用方法：
+# 1. 浏览器打开 https://pan.quark.cn/s/3fa84f948ee1
+# 2. 登录后点击下载
+# 3. 或将cookie导出后使用 curl 下载
+echo "夸克网盘需要登录后手动下载"
+echo "链接: https://pan.quark.cn/s/3fa84f948ee1"
